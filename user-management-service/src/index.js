5c8318e1ca2eba9fac12bfc73322450ab74a8f9f
Sparsh <77487266+iSparshP@users.noreply.github.com>
2024-10-18 13:11:00 +0530

    Refactor database connection and configuration in user management service

--- user-management-service/src/index.js
+++ user-management-service/src/index.js
@@ -1,21 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const connectDB = require('../config/config.js');
 const userRoutes = require('./routes/userRoutes.js');
 const profileRoutes = require('./routes/profileRoutes.js');
 
-dotenv.config();
-
 const app = express();
-app.use(express.json());
+connectDB();
 
-mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.log(err));
+app.use(express.json());
 
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/users', profileRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
